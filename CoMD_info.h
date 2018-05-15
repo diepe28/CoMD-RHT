6474184a29de0f3142ffc21faaeb36875bef8660
diego <diepe2892@gmail.com>
2018-05-15 08:23:57 -0600

    Adding initial files

---
+++ CoMD_info.h
@@ -0,0 +1,15 @@
+#ifndef CoMD_info_hpp
+#define CoMD_info_hpp
+
+#define CoMD_VARIANT "CoMD-mpi"
+#define CoMD_HOSTNAME "diego-Latitude-E7470"
+#define CoMD_KERNEL_NAME "'Linux'"
+#define CoMD_KERNEL_RELEASE "'4.13.0-32-generic'"
+#define CoMD_PROCESSOR "'x86_64'"
+
+#define CoMD_COMPILER "'/usr/bin/mpicc'"
+#define CoMD_COMPILER_VERSION "'gcc (Ubuntu 7.2.0-13ubuntu1~16.04.york0) 7.2.0'"
+#define CoMD_CFLAGS "'-std=c99 -DDOUBLE -DDO_MPI -g -O5   '"
+#define CoMD_LDFLAGS "' -lm '"
+
+#endif
