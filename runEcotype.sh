97e1c6aabc54364e51cf7802a287154dfc8d56c6
diego <diepe2892@gmail.com>
2018-07-23 09:37:42 -0600

    Updating runEcotype script

--- runEcotype.sh
+++ runEcotype.sh
@@ -32,28 +32,28 @@ mpirun -np 1 CoMD-WANG-JV -e -i 1 -j 1 -k 1 -x 30 -y 30 -z 30 5 2 0 2 > replicat
 mpirun -np 1 IMP-CoMD-WANG-JV -e -i 1 -j 1 -k 1 -x 30 -y 30 -z 30 5 2 0 2 > IMP-replicated-wang-jv-1rank-noHT.txt
 
 echo Running baseline 20 ranks
-mpirun -np 20 CoMD-WANG -e -i 5 -j 2 -k 2 -x 70 -y 70 -z 70 5 > mybaseline-20rank.txt
+mpirun -np 20 CoMD-WANG -e -i 5 -j 2 -k 2 -x 80 -y 80 -z 80 5 > mybaseline-20rank.txt
 
 echo Running replicated wang 20 rank noHT
-mpirun -np 20 CoMD-WANG -e -i 5 -j 2 -k 2 -x 70 -y 70 -z 70 5 40 0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 > replicated-wang-20rank-noHT.txt
-mpirun -np 20 IMP-CoMD-WANG -e -i 5 -j 2 -k 2 -x 70 -y 70 -z 70 5 40 0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 > IMP-replicated-wang-20rank-noHT.txt
+mpirun -np 20 CoMD-WANG -e -i 5 -j 2 -k 2 -x 80 -y 80 -z 80 5 40 0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 > replicated-wang-20rank-noHT.txt
+mpirun -np 20 IMP-CoMD-WANG -e -i 5 -j 2 -k 2 -x 80 -y 80 -z 80 5 40 0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 > IMP-replicated-wang-20rank-noHT.txt
 
 echo Running replicated wang 20 rank HT
-mpirun -np 20 CoMD-WANG -e -i 5 -j 2 -k 2 -x 70 -y 70 -z 70 5 40 0 20 2 22 4 24 6 26 8 28 10 30 12 32 14 34 16 36 18 38 1 21 3 23 5 25 7 27 9 29 11 31 13 33 15 35 17 37 19 39 > replicated-wang-20rank-HT.txt
-mpirun -np 20 IMP-CoMD-WANG -e -i 5 -j 2 -k 2 -x 70 -y 70 -z 70 5 40 0 20 2 22 4 24 6 26 8 28 10 30 12 32 14 34 16 36 18 38 1 21 3 23 5 25 7 27 9 29 11 31 13 33 15 35 17 37 19 39 > IMP-replicated-wang-20rank-HT.txt
+mpirun -np 20 CoMD-WANG -e -i 5 -j 2 -k 2 -x 80 -y 80 -z 80 5 40 0 20 2 22 4 24 6 26 8 28 10 30 12 32 14 34 16 36 18 38 1 21 3 23 5 25 7 27 9 29 11 31 13 33 15 35 17 37 19 39 > replicated-wang-20rank-HT.txt
+mpirun -np 20 IMP-CoMD-WANG -e -i 5 -j 2 -k 2 -x 80 -y 80 -z 80 5 40 0 20 2 22 4 24 6 26 8 28 10 30 12 32 14 34 16 36 18 38 1 21 3 23 5 25 7 27 9 29 11 31 13 33 15 35 17 37 19 39 > IMP-replicated-wang-20rank-HT.txt
 
 echo Running replicated wang with var grouping 20 rank noHT
-mpirun -np 20 CoMD-WANG-VG -e -i 5 -j 2 -k 2 -x 70 -y 70 -z 70 5 40 0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 > replicated-wang-vg-20rank-noHT.txt
-mpirun -np 20 IMP-CoMD-WANG-VG -e -i 5 -j 2 -k 2 -x 70 -y 70 -z 70 5 40 0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 > IMP-replicated-wang-vg-20rank-noHT.txt
+mpirun -np 20 CoMD-WANG-VG -e -i 5 -j 2 -k 2 -x 80 -y 80 -z 80 5 40 0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 > replicated-wang-vg-20rank-noHT.txt
+mpirun -np 20 IMP-CoMD-WANG-VG -e -i 5 -j 2 -k 2 -x 80 -y 80 -z 80 5 40 0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 > IMP-replicated-wang-vg-20rank-noHT.txt
 
 echo Running replicated wang with var grouping 20 rank HT
-mpirun -np 20 CoMD-WANG-VG -e -i 5 -j 2 -k 2 -x 70 -y 70 -z 70 5 40 0 20 2 22 4 24 6 26 8 28 10 30 12 32 14 34 16 36 18 38 1 21 3 23 5 25 7 27 9 29 11 31 13 33 15 35 17 37 19 39 > replicated-wang-vg-20rank-HT.txt
-mpirun -np 20 IMP-CoMD-WANG-VG -e -i 5 -j 2 -k 2 -x 70 -y 70 -z 70 5 40 0 20 2 22 4 24 6 26 8 28 10 30 12 32 14 34 16 36 18 38 1 21 3 23 5 25 7 27 9 29 11 31 13 33 15 35 17 37 19 39 > IMP-replicated-wang-vg-20rank-HT.txt
+mpirun -np 20 CoMD-WANG-VG -e -i 5 -j 2 -k 2 -x 80 -y 80 -z 80 5 40 0 20 2 22 4 24 6 26 8 28 10 30 12 32 14 34 16 36 18 38 1 21 3 23 5 25 7 27 9 29 11 31 13 33 15 35 17 37 19 39 > replicated-wang-vg-20rank-HT.txt
+mpirun -np 20 IMP-CoMD-WANG-VG -e -i 5 -j 2 -k 2 -x 80 -y 80 -z 80 5 40 0 20 2 22 4 24 6 26 8 28 10 30 12 32 14 34 16 36 18 38 1 21 3 23 5 25 7 27 9 29 11 31 13 33 15 35 17 37 19 39 > IMP-replicated-wang-vg-20rank-HT.txt
 
 echo Running replicated wang just volatiles grouping 20 ranks noHT
-mpirun -np 20 CoMD-WANG-JV -e -i 5 -j 2 -k 2 -x 70 -y 70 -z 70 5 40 0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 > replicated-wang-jv-20rank-noHT.txt
-mpirun -np 20 IMP-CoMD-WANG-JV -e -i 5 -j 2 -k 2 -x 70 -y 70 -z 70 5 40 0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 > IMP-replicated-wang-jv-20rank-noHT.txt
+mpirun -np 20 CoMD-WANG-JV -e -i 5 -j 2 -k 2 -x 80 -y 80 -z 80 5 40 0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 > replicated-wang-jv-20rank-noHT.txt
+mpirun -np 20 IMP-CoMD-WANG-JV -e -i 5 -j 2 -k 2 -x 80 -y 80 -z 80 5 40 0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 > IMP-replicated-wang-jv-20rank-noHT.txt
 
 echo Running replicated wang just volatiles grouping 20 ranks HT
-mpirun -np 20 CoMD-WANG-JV -e -i 5 -j 2 -k 2 -x 70 -y 70 -z 70 5 40 0 20 2 22 4 24 6 26 8 28 10 30 12 32 14 34 16 36 18 38 1 21 3 23 5 25 7 27 9 29 11 31 13 33 15 35 17 37 19 39 > replicated-wang-jv-20rank-HT.txt    
-mpirun -np 20 IMP-CoMD-WANG-JV -e -i 5 -j 2 -k 2 -x 70 -y 70 -z 70 5 40 0 20 2 22 4 24 6 26 8 28 10 30 12 32 14 34 16 36 18 38 1 21 3 23 5 25 7 27 9 29 11 31 13 33 15 35 17 37 19 39 > IMP-replicated-wang-jv-20rank-HT.txt    
+mpirun -np 20 CoMD-WANG-JV -e -i 5 -j 2 -k 2 -x 80 -y 80 -z 80 5 40 0 20 2 22 4 24 6 26 8 28 10 30 12 32 14 34 16 36 18 38 1 21 3 23 5 25 7 27 9 29 11 31 13 33 15 35 17 37 19 39 > replicated-wang-jv-20rank-HT.txt    
+mpirun -np 20 IMP-CoMD-WANG-JV -e -i 5 -j 2 -k 2 -x 80 -y 80 -z 80 5 40 0 20 2 22 4 24 6 26 8 28 10 30 12 32 14 34 16 36 18 38 1 21 3 23 5 25 7 27 9 29 11 31 13 33 15 35 17 37 19 39 > IMP-replicated-wang-jv-20rank-HT.txt    
